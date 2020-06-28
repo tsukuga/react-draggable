8d4699450b47a2fb7ead3e7c461606a3a6eb1e43
tsukuga <tsukugaryuga0828@gmail.com>
2020-06-28 21:35:12 +0900

    2 files modified, 1 added

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
+import * as History from 'history';
+import createStore from './reducks/store/store';
+import * as serviceWorker from './serviceWorker';
+import {ErrorBoundary} from "./components/UIkit";
+import {MuiThemeProvider} from '@material-ui/core/styles';
+import App from './App';
+
 import Draggable from './Hello';
 import './style.css';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'React'
-    };
-  }
+// connected-react-router - action経由でルーティングが可能、push,replace..
+// historyを強化
+const history = History.createBrowserHistory();
+export const store = createStore(history);
 
-  render() {
-    return (
-      <div>
-        <Draggable/>
-        <p>
-          
-        </p>
-      </div>
-    );
-  }
-}
+render(
+    <Provider store={store}>
+    <App />
+    </Provider>,
+    document.getElementById('root'),
+)
 
 render(<App />, document.getElementById('root'));
--- reducks/store/store.js
+++ reducks/store/store.js
@@ -4,3 +4,11 @@ import {
   applyMiddleware
 } from 'redux';
 
+import { createLogger } from 'redux-logger'
+import thunk from 'redux-thunk';
+import {connectRouter, routerMiddleware} from 'connected-react-router'
+
+// Import reducers
+import {LoadingReducer} from '../loading/reducers';
+import {ProductsReducer} from '../products/reducers';
+import {UsersReducer} from '../users/reducers';
