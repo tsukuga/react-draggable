6c3095e86457e14c47115c54c7ef71f766984012
tsukuga <tsukugaryuga0828@gmail.com>
2020-06-28 11:52:37 +0900

    1 file modified

--- Hello.js
+++ Hello.js
@@ -7,6 +7,11 @@ const Draggable = () =>{
   const [infos,setPostion] = useState(
     {
      width:100,height:200,x:100,y:200,
+     style: {
+        textAlign: "center",
+        fontWeight: 'normal',
+        visible: false
+      },
     }
   );
 
@@ -15,7 +20,7 @@ const Draggable = () =>{
   return( 
     <>
        <Rnd
-        className="box"
+        className="box"　style={infos.style}
         size={{ width: infos.width, height: infos.height }}
         position={{ x: infos.x, y: infos.y }}
         onDragStop={(e,d)=>{
@@ -31,18 +36,23 @@ const Draggable = () =>{
         resizeGrid={[1, 1]}
         dragGrid={[10, 10]}
          >
-        
+        疲れた
         </Rnd>
 
         <Resize resize={()=>setPostion({x:100,y:200})}/>
+        <ChangeStyle Bold={()=>setPostion({style：{center:'center'}})/>
+
     </>
   )
 }
 
 const Resize = (props)=>{
-
     return <button className="box" type="button" onClick={props.resize}>サイズ変更</button>
-  
+}
+
+
+const ChangeStyle = (props)=>{
+    return <button className="box" type="button" onClick={props.resize}>B</button>
 }
 
 export default Draggable
